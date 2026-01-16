9ebc0b4ecd3b36b3a80bbf570f5ea178ae2c2c4a
Daniel Who? <whodaniel@yahoo.com>
2026-01-15 22:16:30 -0500

    fix(frontend): remove duplicate getFirestore import

--- apps/frontend/src/lib/firebase.ts
+++ apps/frontend/src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+
 import { GoogleAuthProvider } from 'firebase/auth';
 import { signInWithPopup } from 'firebase/auth';
 
