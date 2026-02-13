789e345da8cc53e7848dc61f4ea680657aff397f
Daniel Who? <whodaniel@yahoo.com>
2026-02-13 15:13:40 -0500

    refactor: explicitly configure DTS generation to use tsconfig.json.

--- packages/ui-consolidated/tsup.config.ts
+++ packages/ui-consolidated/tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: {
+    tsconfig: './tsconfig.json',
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
