417a42b60124b4b85b43a95b73eca4c891c624e2
Daniel Who? <whodaniel@yahoo.com>
2026-03-18 00:00:39 -0400

    feat: seed poker access rules

--- apps/api/src/modules/access/access.module.ts
+++ apps/api/src/modules/access/access.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { DrizzleModule } from '@the-new-fuse/database/drizzle';
 import { AuthModule } from '../auth/auth.module';
 import { BillingModule } from '../billing/billing.module';
+import { AccessBootstrapService } from './access-bootstrap.service';
 import { AccessController } from './access.controller';
 import { AccessService } from './access.service';
 
 @Module({
   imports: [DrizzleModule, AuthModule, BillingModule],
   controllers: [AccessController],
-  providers: [AccessService],
+  providers: [AccessService, AccessBootstrapService],
   exports: [AccessService],
 })
 export class AccessModule {}
